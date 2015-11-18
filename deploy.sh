16e8d7395dca789151555c89f28c719121eaa1e1
TAMURA Tomohiro <osone3@gmail.com>
2015-11-18 14:53:04 +0900

    記事修正

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# https://gohugo.io/tutorials/github-pages-blog/
+# の deploy.sh
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo -t hugo-zen # if using a theme, replace by `hugo -t <yourtheme>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:tamuratomohiro/tamuratomohiro.github.io.git master
